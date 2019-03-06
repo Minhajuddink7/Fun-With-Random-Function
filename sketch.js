0d21e7626bd78c197422e0a364432a9105564377
Minhajuddink7 <36264830+Minhajuddink7@users.noreply.github.com>
2019-03-06 11:05:31 +0530

    First Commit

---
+++ sketch.js
@@ -0,0 +1,30 @@
+function setup() {
+  // put setup code here
+var x;
+createCanvas(800,600);
+
+
+background(100);
+}
+
+
+function draw() {
+  // put drawing code here
+
+var x=random(0,width);
+var y=random(0,height);
+var r=random(0,255);
+var g=random(0,255);
+var b=random(0,255);
+var s=random(10,40);
+fill(r,g,b);
+noStroke();
+ellipse(x,y,s,s);
+
+} 
+function mousePressed(){
+var r=random(0,255);
+var g=random(0,255);
+var b=random(0,255);
+background(r,g,b);
+}
